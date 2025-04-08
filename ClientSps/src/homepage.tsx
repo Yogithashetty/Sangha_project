dbeaa80c5f4f9ec1a91abfba251af5ded1087f21
Yogitha shetty <yogithagc2004@gmail.com>
2025-04-08 10:23:50 +0530

    changes in home page

--- ClientSps/src/homepage.tsx
+++ ClientSps/src/homepage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../src/Styling/Prant.css";
@@ -10,41 +10,28 @@ interface Prant {
 
 function HomePage() {
   const [prants, setPrants] = useState<Prant[]>([]);
-  const [fetched, setFetched] = useState(false);
   const navigate = useNavigate();
 
-  const fetchPrants = () => {
+  useEffect(() => {
     axios.get("http://localhost:4000/api/prants").then((res) => {
       setPrants(res.data);
-      setFetched(true);
     });
-  };
+  }, []);
 
   const handleClick = (prant: Prant) => {
-    navigate(`/vibhag/${prant._id}`, { state: { prantName: prant.name } });
+    navigate(`/vibhag/${prant._id}`);
   };
 
   return (
     <div className="page-container">
       <h2>ಪ್ರಾಂತವನ್ನು ಆಯ್ಕೆಮಾಡಿ</h2>
-
-      {/* Fetch Prants Button */}
-      {!fetched && (
-        <button className="fetch-btn" onClick={fetchPrants}>
-          Fetch Prants
-        </button>
-      )}
-
-      {/* Prant Buttons */}
-      {fetched && (
-        <div className="button-group">
-          {prants.map((prant) => (
-            <button key={prant._id} onClick={() => handleClick(prant)}>
-              {prant.name}
-            </button>
-          ))}
-        </div>
-      )}
+      <div className="button-group">
+        {prants.map((prant) => (
+          <button key={prant._id} onClick={() => handleClick(prant)}>
+            {prant.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
